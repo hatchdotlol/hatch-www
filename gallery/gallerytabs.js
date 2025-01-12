875a1dfc111d17f832250eef96c0ce82c3ab20db
return-misaPuding <70533713+return-misaPuding@users.noreply.github.com>
2025-01-12 23:31:00 +0100

    fix DOM manipulation
    "wahhh, the ID in getElementByID is actually Id!!!! i shall refuse to work"
    🎲

--- gallery/gallerytabs.js
+++ gallery/gallerytabs.js
@@ -2,7 +2,7 @@ const params = new URLSearchParams(location.search);
 const GID = params.get("id");
 console.log("Gallery ID is "+GID)
 const gurl = "dev.hatch.lol/gallery/";
-document.getElementByID("galleryProjects").href = gurl+"?id="+GID;
-document.getElementByID("galleryComments").href = gurl+"comments/?id="+GID;
-document.getElementByID("galleryMembers").href = gurl+"members/?id="+GID
-document.getElementByID("galleryInfo").href = gurl+"info/?id="+GID
+document.getElementById("galleryProjects").href = gurl+"?id="+GID;
+document.getElementById("galleryComments").href = gurl+"comments/?id="+GID;
+document.getElementById("galleryMembers").href = gurl+"members/?id="+GID
+document.getElementById("galleryInfo").href = gurl+"info/?id="+GID
