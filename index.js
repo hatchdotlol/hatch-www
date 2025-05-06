27f20e976ebbdcff64c70ea5e650b4cb8998fbb0
Jab11n <jabinstech.net@gmail.com>
2025-05-05 22:26:13 -0400

    little improvements to the current homepage

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const token = localStorage["token"];
 
 if (token) {
-  fetch("https://api.hatch.lol/auth/me", {
-    headers: {
-      Token: token,
-    },
-  }).then((res) => {
-    res.json().then((data) => {
-      document.querySelector("#welcomename").innerHTML =
-        `Welcome back, ${data.displayName}!`;
-      document
-        .querySelector("#welcomepfp")
-        .setAttribute("src", `https://api.hatch.lol${data.profilePicture}?size=100`);
-      document.body.classList.remove("loading");
+    fetch("https://api.hatch.lol/auth/me", {
+        headers: {
+            Token: token,
+        },
+    }).then((res) => {
+        res.json().then((data) => {
+            document.querySelector(
+                "#welcomename"
+            ).innerHTML = `Welcome back, ${data.displayName}!`;
+            document
+                .querySelector("#welcomepfp")
+                .setAttribute(
+                    "src",
+                    `https://api.hatch.lol${data.profilePicture}?size=100`
+                );
+            document.body.classList.remove("loading");
+        });
     });
-  });
 } else {
-  document.querySelector("#welcome").style.display = "none";
+    document.querySelector("#welcomename").innerHTML = `Welcome to Hatch!`;
+    document.getElementById("welcomepfp").style.display = "none";
+    // document.querySelector("#welcome").style.display = "none";
 }
