305b4341b3fb67202f5ef87303d06b6a6aaa9d47
raynecloudy <rayne@raynec.dev>
2025-06-18 15:04:27 -0700

    fix race case

--- email-confirmed/email-confirmed.js
+++ email-confirmed/email-confirmed.js
@@ -1,14 +1,12 @@
-let client_username;
-if (localStorage.getItem("token")) {
-    fetch("https://api.hatch.lol/auth/me", {
-        headers: {
-            Token: localStorage.getItem("token"),
-        },
-    }).then((res) =>
-        res.json().then((data) => {
-            client_username = data.name;
-        })
-    );
-}
-document.getElementById("welcomebanner").textContent =
-    "Welcome to Hatch, " + client_username + "!";
+(async () => {
+    let client_username;
+    if (localStorage.getItem("token")) {
+        client_username = (await (await fetch("https://api.hatch.lol/auth/me", {
+            headers: {
+                Token: localStorage.getItem("token"),
+            },
+        })).json()).name;
+    }
+    document.getElementById("welcomebanner").textContent =
+        "Welcome to Hatch, " + client_username + "!";
+});
