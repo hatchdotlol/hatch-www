e014f6ac33497695b8e9c10e58229d5e57224576
raynecloudy <rayne@raynec.dev>
2025-07-11 19:58:34 -0700

    crazy stuff

--- header.js
+++ header.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     const links = navLinks.querySelectorAll("a");
 
     function moveBubble(target) {
+        bubble.classList.remove("hidden");
         const rect = target.getBoundingClientRect();
         const parentRect = navLinks.getBoundingClientRect();
         bubble.style.width = rect.width + 16 + "px";
@@ -14,16 +15,18 @@ document.addEventListener("DOMContentLoaded", async () => {
         bubble.style.left = rect.left - parentRect.left - 8 + "px";
         bubble.style.top = rect.top - parentRect.top - 4 + "px";
     }
-
-    const cur = navLinks.querySelector(".nav-link-cur");
-    if (cur) moveBubble(cur);
-
+    
     links.forEach((link) => {
+        if (link.href === location.href || link.href === location.href.slice(0, location.href.length - 1)) link.className = "nav-link-cur";
         link.addEventListener("mouseenter", () => moveBubble(link));
         link.addEventListener("focus", () => moveBubble(link));
     });
+    
+    const cur = navLinks.querySelector(".nav-link-cur");
+    if (cur) moveBubble(cur);
 
     navLinks.addEventListener("mouseleave", () => {
         if (cur) moveBubble(cur);
+        else bubble.classList.add("hidden");
     });
 });
