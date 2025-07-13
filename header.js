5e86b8072f280cdb88a79df9d5be0e81e49443f1
Jab11n <jabinstech.net@gmail.com>
2025-07-13 18:08:57 -0400

    userbox! (contains notifications, profile button, settings button, and logout btn)

--- header.js
+++ header.js
@@ -1,19 +1,36 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    document.getElementById("header").innerHTML = await (await fetch("/components/header.html")).text();
+    document.getElementById("header").innerHTML = await (
+        await fetch("/components/header.html")
+    ).text();
+    document.getElementById("userbox").innerHTML = await (
+        await fetch("/components/userbox.html")
+    ).text();
 
     if (localStorage.getItem("token")) {
         const res = await fetch("https://apiv2.hatch.lol/auth/me", {
             headers: {
-                Token: localStorage.getItem("token")
-            }
+                Token: localStorage.getItem("token"),
+            },
         });
         if (res.ok) {
             window.user = await res.json();
             document.getElementById("header").classList.add("logged-in");
-            document.querySelector("#nav-user img").src = `https://apiv2.hatch.lol/uploads/pfp/${user.id}.png?size=30`;
-            document.querySelector("#nav-user span").innerText = user.displayName ?? user.username;
+            document.querySelector(
+                "#nav-user img"
+            ).src = `https://apiv2.hatch.lol/uploads/pfp/${user.id}.png?size=30`;
+            document.querySelector("#nav-user span").innerText =
+                user.displayName ?? user.username;
         }
     }
+
+    const userbox = document.getElementById("userbox");
+    document.getElementById("nav-user").addEventListener("click", () => {
+        if (userbox.style.display == "block") {
+            userbox.style.display = "none";
+        } else {
+            userbox.style.display = "block";
+        }
+    });
 
     const navLinks = document.getElementById("nav-links");
     const bubble = document.getElementById("nav-bubble");
@@ -28,13 +45,17 @@ document.addEventListener("DOMContentLoaded", async () => {
         bubble.style.left = rect.left - parentRect.left - 8 + "px";
         bubble.style.top = rect.top - parentRect.top - 4 + "px";
     }
-    
+
     links.forEach((link) => {
-        if (link.href === location.href || link.href === location.href.slice(0, location.href.length - 1)) link.className = "nav-link-cur";
+        if (
+            link.href === location.href ||
+            link.href === location.href.slice(0, location.href.length - 1)
+        )
+            link.className = "nav-link-cur";
         link.addEventListener("mouseenter", () => moveBubble(link));
         link.addEventListener("focus", () => moveBubble(link));
     });
-    
+
     const cur = navLinks.querySelector(".nav-link-cur");
     if (cur) moveBubble(cur);
 
